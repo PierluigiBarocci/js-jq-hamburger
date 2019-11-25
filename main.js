6cb20fb663e1c55a851df691b44bc1d892ae40cd
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2019-11-25 15:18:23 +0100

    Creazione File da Modificare

---
+++ main.js
@@ -0,0 +1,4 @@
+// Partendo dai file che trovate in questa repo: https://bitbucket.org/booleancareers/ex-hamburger-menu-base
+// aggiungere le istruzioni jQuery necessarie e sufficienti per attivare l'hamburger menu.
+// La struttura html e lo stile presente contengono già tutto l'occorrente per l'hamburger menu, ciò che dovete fare è capire com'è predisposto il tutto e incastrare le istruzioni js corrette per mostrare e nascondere l'hamburger menu su mobile.
+// Nome repo: js-jq-hamburger
