d65446f378b7db15fbe0ef3204dc4cc900daf96d
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2019-11-25 15:32:05 +0100

    Esercizio Terminato - erano letteralmente due righe

--- main.js
+++ main.js
@@ -2,3 +2,15 @@
 // aggiungere le istruzioni jQuery necessarie e sufficienti per attivare l'hamburger menu.
 // La struttura html e lo stile presente contengono già tutto l'occorrente per l'hamburger menu, ciò che dovete fare è capire com'è predisposto il tutto e incastrare le istruzioni js corrette per mostrare e nascondere l'hamburger menu su mobile.
 // Nome repo: js-jq-hamburger
+
+
+// il selector per l'hamburger da clickare:   .header-right > a
+// il selector per il menu che deve apparire:   .hamburger-menu
+// il selector per la X che deve chiudere .hamburger-menu:    .close
+
+$('.header-right > a').click(function(){
+    $('.hamburger-menu').show();
+});
+$('.close').click(function(){
+    $('.hamburger-menu').hide();
+});
